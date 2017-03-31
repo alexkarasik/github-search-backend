d2124d68dc069cec362db8aaf22c2f87c4ebcc4a
kinkorea <alexkarasik89@gmail.com>
2017-03-30 21:35:17 -0400

    Changed the create script for bookmarked repos.

--- scripts/bookmarked_repo/create.sh
+++ scripts/bookmarked_repo/create.sh
@@ -1,15 +1,18 @@
 API="${API_ORIGIN:-http://localhost:4741}"
-URL_PATH="/favorites"
+URL_PATH="/bookmarked_repos"
+TOKEN="BAhJIiUyNWI1MzYwYTM5ZTk1MzE1NzJlNjJlNzE3YzM3ZjJiNQY6BkVG--7d5824e14b6bd930dc73a77cc73ed12f83fae824"
 curl "${API}${URL_PATH}" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
-  --header "Authorization: Token token=$TOKEN" \
+  --header "Authorization: Token token=${TOKEN}" \
   --data '{
     "repo": {
-      "name": "'"${NAME}"'",
-      "link": "'"${LINK}"'",
-      "language": "'"${LANGUAGE}"'"
+      "title": "'"${TITLE}"'",
+      "language": "'"${LANGUAGE}"'",
+      "score": "'"${SCORE}"'",
+      "forks": "'"${FORKS}"'",
+      "description": "'"${DESCRIPTION}"'"
     }
   }'
 
